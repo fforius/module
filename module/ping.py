3803ff8929a37cd3a9b3a0344526192086c82623
fforius <60130637+fforius@users.noreply.github.com>
2020-03-19 20:54:52 +0700

    nlmn

---
+++ module/ping.py
@@ -0,0 +1,14 @@
+from telethon import events
+from datetime import datetime
+from userbot.events import register
+
+
+@register(pattern="^.pi")
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    await event.edit("Pong!")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit("Pong!\n{}".format(ms))
